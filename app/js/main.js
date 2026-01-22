3cabddbe9e96d610602655e9ed2ca09284ac40ea
nataliia13789 <nataliiaokhmat.com>
2026-01-22 21:01:47 +0100

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
- 
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 40, 
+  loop: true,
+  navigation: {
+    nextEl: '.arrow-next',
+    prevEl: '.arrow-prev',
+  },
+});
